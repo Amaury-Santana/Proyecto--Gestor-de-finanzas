d94b2c49b073bab825eb3f912abb1323ce2aff57
Amaury-Santana <amaurysantana070@gmail.com>
2025-05-22 17:54:21 -0600

    transaccion.cpp

---
+++ transaccion.cpp
@@ -0,0 +1,31 @@
+#include "transaccion.h"
+
+Transaccion::Transaccion() {
+    monto = 0.0;
+    fecha = "";
+    descripcion = "";
+    categoria = nullptr;
+}
+
+Transaccion::Transaccion(double _monto, string _fecha, string _descripcion, Categoria* _categoria) {
+    monto = _monto;
+    fecha = _fecha;
+    descripcion = _descripcion;
+    categoria = _categoria;
+}
+
+double Transaccion::getMonto() {
+    return monto;
+}
+
+string Transaccion::getFecha() {
+    return fecha;
+}
+
+string Transaccion::getDescripcion() {
+    return descripcion;
+}
+
+Categoria* Transaccion::getCategoria() {
+    return categoria;
+}

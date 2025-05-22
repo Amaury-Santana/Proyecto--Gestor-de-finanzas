0aef031f1815ab51e974fa8a86a632c592a76673
Amaury-Santana <amaurysantana070@gmail.com>
2025-05-22 17:58:11 -0600

    gasto.cpp

---
+++ gasto.cpp
@@ -0,0 +1,32 @@
+#include "gasto.h"
+
+Gasto::Gasto() : Transaccion() {
+    tipoPago = "";
+    esDeducible = false;
+}
+
+Gasto::Gasto(double _monto, string _fecha, string _descripcion, Categoria* _categoria, string _tipoPago, bool _esDeducible)
+: Transaccion(_monto, _fecha, _descripcion, _categoria) {
+    tipoPago = _tipoPago;
+    esDeducible = _esDeducible;
+}
+
+string Gasto::getTipoPago() {
+    return tipoPago;
+}
+
+bool Gasto::getEsDeducible() {
+    return esDeducible;
+}
+//Implemento el método que sobrescribe el virtual
+string Gasto::mostrarDetalles() {
+    return "Gasto de " + to_string(monto) + " usando " + tipoPago;
+}
+// Cálculo de impuesto solo si el gasto es deducible
+double Gasto::calcularImpuesto() {
+    if (esDeducible) {
+        return monto * 0.08;
+    } else {
+        return 0.0;
+    }
+}

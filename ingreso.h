0c4b1a76766005d5456e6388b8f333413f5d99ec
Amaury-Santana <amaurysantana070@gmail.com>
2025-05-22 17:56:28 -0600

    ingreso.h

---
+++ ingreso.h
@@ -0,0 +1,22 @@
+#ifndef INGRESO_H
+#define INGRESO_H
+
+#include "transaccion.h"
+using namespace std;
+
+//Heredación
+class Ingreso : public Transaccion {
+private:
+    string fuente;
+
+public:
+    Ingreso();
+    Ingreso(double _monto, string _fecha, string _descripcion, Categoria* _categoria, string _fuente);
+
+    string getFuente();
+
+    string mostrarDetalles() override;
+    double calcularImpuesto() override;
+};
+
+#endif

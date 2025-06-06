665ea94fae0f82b13383a6520efc8fae41224631
Amaury-Santana <amaurysantana070@gmail.com>
2025-06-05 19:47:11 -0600

    Update resumenmensual.cpp

--- resumenmensual.cpp
+++ resumenmensual.cpp
@@ -1,29 +1,38 @@
+// Implementación de los métodos de ResumenMensual
+// Agrega transacciones, calcula balance e imprime resumen con impuestos y deducciones que es lo que aparece en el resumen mensual
+
 #include "resumenmensual.h"
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 ResumenMensual::ResumenMensual() {
     mes = 0;
     year = 0;
+    contador = 0;
 }
 
 ResumenMensual::ResumenMensual(int _mes, int _year) {
     mes = _mes;
     year = _year;
+    contador = 0;
 }
 
 void ResumenMensual::agregarTransaccion(Transaccion* t) {
-    transacciones.push_back(t);
+    if (contador < 100) {
+        transacciones[contador] = t;
+        contador++;
+    }
 }
 
 double ResumenMensual::calcularBalance() {
     double total = 0;
 
-    for (Transaccion* t : transacciones) {
-        if (t->esIngreso()) {
-            total += t->getMonto();
+    for (int i = 0; i < contador; i++) {
+        if (transacciones[i]->esIngreso()) {
+            total += transacciones[i]->getMonto();
         } else {
-            total -= t->getMonto();
+            total -= transacciones[i]->getMonto();
         }
     }
 
@@ -34,35 +43,33 @@ double ResumenMensual::calcularImpuestos() {
     double impuestos = 0;
     double deducciones = 0;
 
-    for (Transaccion* t : transacciones) {
-        if (t->esIngreso()) {
-            impuestos += t->calcularImpuesto();
+    for (int i = 0; i < contador; i++) {
+        if (transacciones[i]->esIngreso()) {
+            impuestos += transacciones[i]->calcularImpuesto();
         } else {
-            deducciones += t->calcularImpuesto();
+            deducciones += transacciones[i]->calcularImpuesto();
         }
     }
 
     return impuestos - deducciones;
 }
 
-
-
 void ResumenMensual::mostrarResumen() {
+    cout << fixed << setprecision(2);
     cout << "Resumen del mes " << mes << "/" << year << endl;
 
-    for (Transaccion* t : transacciones) {
-        cout << t->mostrarDetalles() << endl;
+    for (int i = 0; i < contador; i++) {
+        cout << transacciones[i]->mostrarDetalles() << endl;
     }
 
-    //Separo los impuestos y deducciones
     double impuestos = 0;
     double deducciones = 0;
 
-    for (Transaccion* t : transacciones) {
-        if (t->esIngreso()) {
-            impuestos += t->calcularImpuesto();
+    for (int i = 0; i < contador; i++) {
+        if (transacciones[i]->esIngreso()) {
+            impuestos += transacciones[i]->calcularImpuesto();
         } else {
-            deducciones += t->calcularImpuesto();
+            deducciones += transacciones[i]->calcularImpuesto();
         }
     }
 
@@ -74,7 +81,10 @@ void ResumenMensual::mostrarResumen() {
     cout << "Balance final de ingresos y gastos: " << calcularBalance() << endl;
 }
 
-
-const vector<Transaccion*>& ResumenMensual::getTransacciones() {
+Transaccion** ResumenMensual::getTransacciones() {
     return transacciones;
+}
+
+int ResumenMensual::getCantidadTransacciones() {
+    return contador;
 }

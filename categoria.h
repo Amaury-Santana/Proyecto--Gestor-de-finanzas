9f2c893d5eaac37c3bc4024b24ffc2fce361dac9
Amaury-Santana <amaurysantana070@gmail.com>
2025-06-05 19:40:06 -0600

    Update categoria.h

--- categoria.h
+++ categoria.h
@@ -1,10 +1,12 @@
+//Clase asociada a transacción para representar una categoría de gasto o ingreso
+
 #ifndef CATEGORIA_H
 #define CATEGORIA_H
 
 #include <string>
 using namespace std;
 
-//Clase asociada a transacción para representar una categoría de gasto o ingreso
+
 class Categoria {
 private:
     string nombre;

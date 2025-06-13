bfd11c9ae0900f7b33369cd3b6497c6f0ce9c492
Amaury-Santana <amaurysantana070@gmail.com>
2025-06-12 21:52:34 -0600

    Update categoria.cpp

--- categoria.cpp
+++ categoria.cpp
@@ -1,18 +1,36 @@
+// categoria.cpp
+
 #include "categoria.h"
+
+
+// Constructor por defecto:
+// Inicializa los atributos nombre y tipo como cadenas vacías.
 
 Categoria::Categoria() {
     nombre = "";
     tipo = "";
 }
 
+
+// Constructor con parámetros:
+// Inicializa los atributos nombre y tipo con los valores dados.
+
 Categoria::Categoria(string _nombre, string _tipo) {
     nombre = _nombre;
     tipo = _tipo;
 }
 
+
+// getNombre()
+// Regresa el nombre de la categoría.
+
 string Categoria::getNombre() {
     return nombre;
 }
+
+
+// getTipo()
+// Regresa el tipo de categoría (por ejemplo: ingreso o gasto).
 
 string Categoria::getTipo() {
     return tipo;

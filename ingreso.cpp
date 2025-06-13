7b50bf487beb6359d0a1795e4b10af0342b06a4c
Amaury-Santana <amaurysantana070@gmail.com>
2025-06-12 21:54:56 -0600

    Update ingreso.cpp

--- ingreso.cpp
+++ ingreso.cpp
@@ -1,32 +1,56 @@
-// Implementación de Ingreso
-// Muestra detalles del ingreso e impone una tasa fija del 10% como impuesto (importante decir que siempre la aplicara para el proyecto)
-
+//ingreso.cpp
 
 #include "ingreso.h"
+
+
+// Constructor por defecto:
+// Inicializa la fuente como cadena vacía.
 
 Ingreso::Ingreso() : Transaccion() {
     fuente = "";
 }
 
-Ingreso::Ingreso(double _monto, string _fecha, string _descripcion, Categoria* _categoria, string _fuente)
-: Transaccion(_monto, _fecha, _descripcion, _categoria) {
+
+// Constructor con parámetros:
+// Inicializa los atributos de un ingreso, incluyendo su fuente.
+
+Ingreso::Ingreso(double _monto, string _fecha, string _descripcion,
+                 Categoria* _categoria, string _fuente)
+    : Transaccion(_monto, _fecha, _descripcion, _categoria) {
     fuente = _fuente;
 }
+
+
+// getFuente()
+// Regresa la fuente de ingreso.
 
 string Ingreso::getFuente() {
     return fuente;
 }
-//Implemento el método que sobreescribe el virtual
+
+
+// mostrarDetalles()
+// Implementación del método sobrescrito.
+// Devuelve un string con los detalles del ingreso.
+
 string Ingreso::mostrarDetalles() {
     return "Ingreso de " + to_string(monto) + " por " + fuente +
-           " [" + categoria->getNombre() + "] - " + fecha + " - " + descripcion;
+           " [" + categoria->getNombre() + "] - " + fecha +
+           " - " + descripcion;
 }
 
 
-//Impuesto fijo del 10% sobre ingresos
+// calcularImpuesto()
+// Aplica un impuesto fijo del 10% sobre el monto.
+
 double Ingreso::calcularImpuesto() {
     return monto * 0.10;
 }
+
+
+// esIngreso()
+// Indica que esta transacción es un ingreso.
+
 bool Ingreso::esIngreso() const {
-    return true; 
+    return true;
 }

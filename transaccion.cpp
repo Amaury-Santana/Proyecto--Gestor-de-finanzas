2f97130c02c0c4e100611a46a1ed5eacc1edb983
Amaury-Santana <amaurysantana070@gmail.com>
2025-06-12 21:58:56 -0600

    Update transaccion.cpp

--- transaccion.cpp
+++ transaccion.cpp
@@ -1,8 +1,9 @@
-// Métodos comunes de la clase Transaccion
-// Getters para monto, fecha, descripción y categoría
+//transaccion.cpp
 
 #include "transaccion.h"
 
+// Constructor por defecto
+// Inicializa valores vacíos o nulos
 Transaccion::Transaccion() {
     monto = 0.0;
     fecha = "";
@@ -10,25 +11,36 @@ Transaccion::Transaccion() {
     categoria = nullptr;
 }
 
-Transaccion::Transaccion(double _monto, string _fecha, string _descripcion, Categoria* _categoria) {
+// Constructor con parámetros
+// Recibe valores y los asigna a atributos
+Transaccion::Transaccion(double _monto, string _fecha,
+                         string _descripcion, Categoria* _categoria) {
     monto = _monto;
     fecha = _fecha;
     descripcion = _descripcion;
     categoria = _categoria;
 }
 
+// getMonto()
+// Retorna el monto asociado a la transacción
 double Transaccion::getMonto() {
     return monto;
 }
 
+// getFecha()
+// Devuelve la fecha en formato string
 string Transaccion::getFecha() {
     return fecha;
 }
 
+// getDescripcion()
+// Devuelve el texto descriptivo de la transacción
 string Transaccion::getDescripcion() {
     return descripcion;
 }
 
+// getCategoria()
+// Devuelve el puntero a la categoría asociada
 Categoria* Transaccion::getCategoria() {
     return categoria;
 }
